bd2ebad681ee3bd6755ffdba3c7f488f5717b01d
Adriano Mingattos <adriano_mingattos@hotmail.com>
2022-11-01 17:50:26 -0300

    Revert "add"
    This reverts commit 5a8a442178a4f60c7100bd7fd059fea5460d555e.

---
+++ seufinanceiro2/README.md
@@ -0,0 +1,1 @@
+# seufinanceiro2
---
+++ seufinanceiro2/frm_principal.cs
@@ -0,0 +1,15 @@
+namespace UI
+{
+    public partial class frm_principal : Form
+    {
+        public frm_principal()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_principal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
